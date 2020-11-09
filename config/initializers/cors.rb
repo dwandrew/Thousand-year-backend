8c26003461fe8dceadcd208a264f5496a7b1b017
dwandrew <danwandrew@gmail.com>
2020-11-09 15:30:38 +0000

    changed origins back for delpoyment to heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@ Rails.application.config.action_controller.forgery_protection_origin_check = fal
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    Access-Control-Allow-Origin: 'https://thousand-year-chronicler.netlify.app'
+    origins 'https://thousand-year-chronicler.netlify.app'
     
 
     resource '*',
