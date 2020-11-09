b34ce03f37c8a0f0932c2bb4f9dc419828d89855
dwandrew <danwandrew@gmail.com>
2020-11-09 15:25:13 +0000

    changed access control origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,11 +8,11 @@ Rails.application.config.action_controller.forgery_protection_origin_check = fal
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://thousand-year-chronicler.netlify.app'
+    Access-Control-Allow-Origin: 'https://thousand-year-chronicler.netlify.app'
     
 
     resource '*',
-      headers: :any, Access-Control-Allow-Origin: 'https://thousand-year-chronicler.netlify.app',
+      headers: :any, 
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
   end
